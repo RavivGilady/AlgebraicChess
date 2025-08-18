49f9b40a0dbebcddc7d84931dbd1d2a12437b2bb
Raviv Gilady <ravivos2137@gmail.com>
2025-08-18 13:44:34 +0300

    prettify

--- backend/players/HumanPlayer.js
+++ backend/players/HumanPlayer.js
@@ -1,50 +1,54 @@
-const logger = require('../utils/logger.js')
+const logger = require("../utils/logger.js");
 
-const Player = require('./Player.js');
+const Player = require("./Player.js");
 
 class HumanPlayer extends Player {
-    constructor(socket, playerDetails) {
-        super();
-        this.socket = socket;
-        this.onMove = null;
-        this.playerDetails = playerDetails;
+  constructor(socket, playerDetails) {
+    super();
+    this.socket = socket;
+    this.onMove = null;
+    this.playerDetails = playerDetails;
+  }
+  sendMessage(...args) {
+    if (this.socket) {
+      this.socket.emit(...args);
     }
-    sendMessage(...args) {
-        if (this.socket) {
-            this.socket.emit(...args);
-        }
-    }
-    setColor(color) {
-        this.sendMessage("color", color);
-    }
+  }
+  setColor(color) {
+    this.sendMessage("color", color);
+  }
 
-    notifyBadMove(move) {
-        this.sendMessage("bad move", move);
-    }
-    gameOver(reason, winner) {
-        this.sendMessage("game Over", reason, winner);
-    }
-    notifyMove(move) {
-        this.sendMessage("move made", move);
-    }
-    setOnMoveCallback(callback) {
-        this.onMove = callback;
-    }
+  notifyBadMove(move) {
+    this.sendMessage("bad move", move);
+  }
+  gameOver(reason, winner) {
+    this.sendMessage("game Over", reason, winner);
+  }
+  notifyMove(move) {
+    this.sendMessage("move made", move);
+  }
+  setOnMoveCallback(callback) {
+    this.onMove = callback;
+  }
 
-    requestMove(moveId) {
-        if (this.onMove == null) {
-            throw new Error("player doesn't have a callback!")
-        }
-        this.sendMessage("make move", moveId)
-
-        this.socket.once(`move ${moveId}`, move => this.onMove({ "move": move, "moveId": moveId }))
+  requestMove(moveId) {
+    if (this.onMove == null) {
+      throw new Error("player doesn't have a callback!");
     }
+    this.sendMessage("make move", moveId);
 
-    getPlayerDetails() {
-        return ({ type: 'human', elo: this.playerDetails.elo, name: this.playerDetails.username })
-    }
+    this.socket.once(`move ${moveId}`, (move) =>
+      this.onMove({ move: move, moveId: moveId })
+    );
+  }
 
+  getPlayerDetails() {
+    return {
+      type: "human",
+      elo: this.playerDetails.elo,
+      name: this.playerDetails.username,
+    };
+  }
 }
 
 module.exports = HumanPlayer;
-
