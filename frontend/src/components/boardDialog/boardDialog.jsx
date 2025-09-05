56a58df63d27c6e804d157116b067ef86ed20cb2
Raviv Gilady <ravivos2137@gmail.com>
2025-09-06 00:30:11 +0300

    center  peek board

--- frontend/src/components/boardDialog/boardDialog.jsx
+++ frontend/src/components/boardDialog/boardDialog.jsx
@@ -15,12 +15,6 @@ import { cn } from "@/lib/utils";
 const BoardDialog = ({ open, onOpenChange }) => {
   const { gameState } = useGame();
 
-  //   useEffect(() => {
-  //     if (open) {
-  //       const timer = setTimeout(() => setOpen(false), 3000); // 3 seconds
-  //       return () => clearTimeout(timer);
-  //     }
-  //   }, [open]);
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="animate-fade-in data-[state=closed]:animate-fade-out">
